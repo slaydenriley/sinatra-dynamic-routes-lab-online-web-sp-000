619b3d48ea0f268edd70982fc6e37eacd2dfb117
Riley Slayden <slaydenriley@gmail.com>
2020-02-12 18:51:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,4 +8,14 @@ class App < Sinatra::Base
   get '/square/:number' do
     (params[:number].to_i ** 2).to_s
   end
+
+  get '/say/:number/:phrase' do
+    answer = ''
+
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+
+    answer
+  end
 end
